fc1337e818146e1dad9dd12e49b92766d9985ae3
André Nascimento Costa <nascimentoandre448@gmail.com>
2022-09-17 16:05:54 -0300

    Create tela.py

---
+++ tela.py
@@ -0,0 +1,31 @@
+import PySimpleGUI as sg 
+
+class TelaPython:
+    def __init__(self):
+        #layout
+        layout = [
+            [sg.Text("Login", size=(5, 0)), sg.Input(size=(20, 0), key='login')],
+            [sg.Text("Senha", size=(5, 0)), sg.Input(size=(20, 0), key='senha')],
+            [sg.Button("New user"), sg.Button("Log in"), sg.Button("Go out")],
+            [sg.Output(size=(20, 10))]
+        ]
+        #janela
+        self.janela = sg.Window("Janela de login").layout(layout)
+        
+        
+    def Iniciar(self):
+        while True:
+            #Extrair os dados da tela
+            self.button, self.values =self.janela.Read()
+        
+            login = self.values['login']
+            senha = self.values['senha']
+            print(f'login: {login}')
+            print(f'senha: {senha}')
+        
+tela = TelaPython()
+tela.Iniciar()
+
+
+
+
